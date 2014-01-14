4415f2c4c2b8c24a3e71810e2983a161c2467236
Brad Feehan <git@bradfeehan.com>
2014-01-14 14:47:56 +1100

    Add deps to install Powerline patched Menlo font

---
+++ Casks/font-menlo-powerline.rb
@@ -0,0 +1,7 @@
+class FontMenloPowerline < Cask
+  url 'https://gist.github.com/raw/1627888/c4e92f81f7956d4ceaee11b5a7b4c445f786dd90/Menlo-ForPowerline.ttc.zip'
+  homepage 'https://powerline.readthedocs.org/en/latest/fontpatching.html'
+  version '6.1d8e1'
+  sha1 '9b317e7597c8acd5de2991175223eeb5cf694fde'
+  font 'Menlo-ForPowerline.ttc'
+end
