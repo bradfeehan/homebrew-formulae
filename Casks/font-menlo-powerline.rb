b3779e316cecf8eb685e44eed8d5e9cec6097f90
Brad Feehan <git@bradfeehan.com>
2015-01-22 10:48:53 +1100

    Use SHA256 instead of outdated SHA1

--- Casks/font-menlo-powerline.rb
+++ Casks/font-menlo-powerline.rb
@@ -2,6 +2,6 @@ cask :v1 => 'font-menlo-powerline' do
   url 'https://gist.github.com/raw/1627888/c4e92f81f7956d4ceaee11b5a7b4c445f786dd90/Menlo-ForPowerline.ttc.zip'
   homepage 'https://powerline.readthedocs.org/en/latest/fontpatching.html'
   version '6.1d8e1'
-  sha1 '9b317e7597c8acd5de2991175223eeb5cf694fde'
+  sha256 'd6c4c9ea1d852a68a1e009350f329c2b2b1c67ab955b5738829e7833ee3b84ef'
   font 'Menlo-ForPowerline.ttc'
 end
