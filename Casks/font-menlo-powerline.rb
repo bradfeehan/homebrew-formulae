c4a9dbd96981a5ab942f1d7b84d2a238f1378ae7
Brad Feehan <git@bradfeehan.com>
2015-01-22 10:21:00 +1100

    Update cask formulae to use new header format

--- Casks/font-menlo-powerline.rb
+++ Casks/font-menlo-powerline.rb
@@ -1,4 +1,4 @@
-class FontMenloPowerline < Cask
+cask :v1 => 'font-menlo-powerline' do
   url 'https://gist.github.com/raw/1627888/c4e92f81f7956d4ceaee11b5a7b4c445f786dd90/Menlo-ForPowerline.ttc.zip'
   homepage 'https://powerline.readthedocs.org/en/latest/fontpatching.html'
   version '6.1d8e1'
