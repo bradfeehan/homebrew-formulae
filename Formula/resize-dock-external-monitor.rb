27b63f43ecf635dbe9281a245b8fe87f904a73f0
Brad Feehan <git@bradfeehan.com>
2016-10-05 10:09:14 +1100

    Fix URL for resize-dock-external-monitor

--- Formula/resize-dock-external-monitor.rb
+++ Formula/resize-dock-external-monitor.rb
@@ -1,6 +1,6 @@
 class ResizeDockExternalMonitor < Formula
   homepage "https://gist.github.com/bradfeehan/5306477"
-  url "https://gist.githubusercontent.com/bradfeehan/5306477/raw/bde26ef9356bf78c92f2717107cb82d11186a10a/resize-dock-external-monitor.sh"
+  url "https://gist.githubusercontent.com/bradfeehan/5306477/raw/72cb3b2140f25a0506fd93aa6a06fa3c6d488d72/resize-dock-external-monitor.sh"
   version "72cb3b2140f25a0506fd93aa6a06fa3c6d488d72"
   sha256 "f539161fb22ca8a26927e4f3e60ffd8dc99368ae98d8f35c9e865e281b2a65eb"
 
