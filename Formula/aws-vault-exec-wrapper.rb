9f3a2858372c5b94e0a1c595144fe5c83532d64d
Brad Feehan <git@bradfeehan.com>
2019-07-26 11:51:09 +1000

    Use v1.0.0 of aws-vault-exec-wrapper

--- Formula/aws-vault-exec-wrapper.rb
+++ Formula/aws-vault-exec-wrapper.rb
@@ -1,11 +1,11 @@
 class AwsVaultExecWrapper < Formula
   desc "Simple wrapper around aws-vault and similar tools"
   homepage "https://github.com/bradfeehan/aws-vault-exec-wrapper"
-  url "https://raw.githubusercontent.com/bradfeehan/aws-vault-exec-wrapper/bb271a1a555a59a31d4a63047a33227cf2715a5d/bin/aws-vault-exec-wrapper"
-  sha256 "f64f1b0bf5a191d64bf8ebfeaf87e65f1f2e9e554747536bf192138ced5a4ff1"
+  url "https://github.com/bradfeehan/aws-vault-exec-wrapper/archive/v1.0.0.tar.gz"
+  sha256 "f5b67cf49a4684d7582811047a21f223fe77b2bea6444c1fdba86ed63b4519e5"
 
   def install
-    bin.install "aws-vault-exec-wrapper"
+    bin.install "bin/aws-vault-exec-wrapper"
   end
 
   test do
