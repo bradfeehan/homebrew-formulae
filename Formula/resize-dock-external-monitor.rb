a3c8c3bc379f0e64a76135e6acefd2ab46f58afa
Brad Feehan <git@bradfeehan.com>
2022-09-12 16:27:15 +1000

    Update resize-dock-external-monitor.rb

--- Formula/resize-dock-external-monitor.rb
+++ Formula/resize-dock-external-monitor.rb
@@ -1,8 +1,8 @@
 class ResizeDockExternalMonitor < Formula
   homepage "https://gist.github.com/bradfeehan/5306477"
-  url "https://gist.githubusercontent.com/bradfeehan/5306477/raw/3bfa4ca47532545e466b2de81a1ed1e37fb0df99/resize-dock-external-monitor.sh"
-  version "3bfa4ca47532545e466b2de81a1ed1e37fb0df99"
-  sha256 "3d0e34b759c795d27f51f83cbfa44f3caee7a3be3a72eea8ad4ef477d7853b88"
+  url "https://gist.githubusercontent.com/bradfeehan/5306477/raw/e80ae5a2823759d183296d92ce52faf93a588646/resize-dock-external-monitor.sh"
+  version "e80ae5a2823759d183296d92ce52faf93a588646"
+  sha256 "7c1be6cf4e0c4b30c0381fc7174d490924ef0057df56f41f6eabaad12de32dd2"
 
   def install
     bin.install "resize-dock-external-monitor.sh"
